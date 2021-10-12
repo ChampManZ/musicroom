4f9de3aa47118eaa315938d872e766318a3ac49d
palmlucksamon <72394952+palmlucksamon@users.noreply.github.com>
2021-10-13 00:53:58 +0700

    button for the join page

--- Frontend/musicplayroom/musico/src/membroom.js
+++ Frontend/musicplayroom/musico/src/membroom.js
@@ -3,24 +3,46 @@ import YouTube from 'react-youtube';
 
 export default function JoinedRoom() {
   var paramst = new URLSearchParams(window.location.search);
- var mykeyroom  = paramst.get('roomid')
+  var mykeyroom  = paramst.get('roomid')
   const[pun, addpun] = useState(0)
+  const [songState, setsongState] = useState("")
+  var [songQueue, setQueue] = useState([""])
+  // const[]
   function play_on_click(){
     console.log("click me")
     addpun(pun+1)
     console.log(pun)
   }
+  function restart(){
+    console.log()
+  }
+  function mute_on_click(){
 
+  }
+  function skip_now(){
+
+  } 
+  const inputSong = (e)=> {
+    setsongState(e.target.value)
+  }
+  const addQueue=()=>{
+    setQueue(prevQ =>  [prevQ, songState])
+    setsongState("")
+  }
     return <div className='joinedroom'>
       <br></br>
       <p>Room ID: {mykeyroom} </p>
       <button onClick= {()=>play_on_click()}> play/pause </button>
-
+      <button onClick= {()=>restart()}> restart </button>
+      <button onClick= {()=>mute_on_click()}> mute/unmute </button>
+      <button onClick= {()=>skip_now()}> skip </button>
+      <br></br>
+      <input onChange={inputSong} value={songState}></input>
+      <button onClick= {()=>addQueue()}> add to queue </button>
 
         
     </div>;
   }
-
 
 
 
